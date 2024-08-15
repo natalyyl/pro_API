0ceeb1f3346870ed6b6f9982cb77d95c7fb0883e
Nataly <naty7160@gmail.com>
2024-08-15 13:32:43 -0300

    subindo com modules

--- api.js
+++ api.js
@@ -1,1 +1,14 @@
-console.log("teste");
+import express from "express";
+
+
+const servidor = express();
+
+servidor.get('helloworld', (req,resp) => {
+    resp.send("Hello world");
+});
+//get o verbo para o endepoint
+// '/título' caminho do endepoint
+// (req,resp) request e response
+// => expressão de função de seta 
+
+servidor.listen(5001, () => console.log('APi subida com sucesso'));
